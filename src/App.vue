1595040d194674b9c3831db9f3dc98bf8a496dcc
fork_web <leshadgrad@mail.ru>
2020-06-18 01:30:17 +0400

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Landing></Landing>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" type="text/scss">
+  @import "./style/main.scss";
 </style>
---
+++ src/components/Landing.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="landing">
+    
+
+    <div class="container menu">
+
+      <div class="menu-logo">
+        <img src="" alt="">
+      </div>
+
+      <div class="menu-nav">
+        <ul>
+          <li><a href="#">Главная</a></li>
+          <li><a href="#">Проекты</a></li>
+          <li><a href="#">Услуги</a></li>
+          <li><a href="#">О компании</a></li>
+          <li><a href="#">Готовые решения</a></li>
+          <li><a href="#">Контакты</a></li>
+        </ul>
+      </div>
+
+      <div class="menu-contact">
+        <div class="menu-contact__item">
+          info@sld-store.ru
+        </div>
+        <div class="menu-contact__item">
+
+        </div>
+      </div>
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+
+// import animateScrollTo from 'animated-scroll-to';
+// import jQuery from "jquery";
+
+export default {
+  name: 'Landing',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
